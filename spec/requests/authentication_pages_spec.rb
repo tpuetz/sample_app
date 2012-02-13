a406d457bda40069169c56af5d4e23bfa9a83af4
Tyson Puetz <tpuetz@gmail.com>
2012-02-12 23:20:25 -0500

    Fisnish user edit, update, index and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,19 +14,17 @@ describe "Authentication" do
       it { should have_selector('div.flash.error', text: 'Invalid') }
     end
 
-  describe "with valid information" do
+      describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { valid_signin(user) }
 
       it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Users',    href: users_path) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end
+  	end
   end
 
   describe "with invalid information" do
